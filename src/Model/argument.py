95d556565e025f90bc6f8f5574453647df196f6a
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-10-07 20:26:51 +0200

    Model files added

---
+++ src/Model/argument.py
@@ -0,0 +1,12 @@
+# TODO: Create Argument class. It should contain the following private variables:
+#  - __id:              str
+#  - __second_id:       str
+#  - __help:            str
+#  - __default:         str
+#  - __required:        bool
+#  - __type:            str
+#  - __action:          str
+#  - __choices:         List[str]
+
+# TODO: Create constructor for Argument class, that sets all variables above.
+# TODO: Create getters for all the variables in Argument class.
---
+++ src/Model/fileinfo.py
@@ -0,0 +1,9 @@
+# TODO: Create FileInfo class. It should contain the following private variables:
+#  - __prog_path:           str
+#  - __prog_name:           str
+#  - __prog_description:    str
+#  - __args:                List[Argument]
+
+# TODO: Create constructor for FileInfo class, that sets all variables above.
+# TODO: Create getters for all the variables in FileInfo class.
+# TODO: Create add_argument(arg: Argument) method.
