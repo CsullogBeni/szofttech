01a59adf24ee1373196e4b06015c424466d06ddb
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-10-07 20:27:56 +0200

    Persistence files added with todos

---
+++ src/Persistence/data_access.py
@@ -0,0 +1,5 @@
+# TODO: Creat DataAccess class, inherit from IDataAccess. Class must contain the following private variables:
+#  - __app_data_path:   str - Consider using app data directory.
+
+# TODO: Create constructor for DataAccess class, that sets all variables above.
+# TODO: Implement the abstract methods: save_config(runnable: FileInfo, data: dict), load_config(runnable: FileInfo), save_main_runnables(runnables: dict), load_main_runnables(), clear_history()
---
+++ src/Persistence/idata_access.py
@@ -0,0 +1,6 @@
+# TODO: Create IDAtaAccess interface that contains the following abstract methods:
+#  - save_config(runnable: FileInfo, data: dict) -> None
+#  - load_config(runnable: FileInfo) -> dict
+#  - save_main_runnables(runnables: data: dict) -> None
+#  - load_main_runnables() -> dict
+#  - clear_history() -> None
