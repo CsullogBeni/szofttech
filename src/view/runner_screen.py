131cf0c84fa29732affd2bf4958d3367933ba879
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-10-09 16:42:50 +0200

    runner screen added with todos

---
+++ src/view/runner_screen.py
@@ -0,0 +1,13 @@
+# TODO: Create RunnerScreen class. It should contain the following private variables:
+#  - __model :       Model
+#  - __widget:      QtWidgets.QStackedWidget
+#  - __scroll_area: QtWidgets.QScrollArea
+#  - __vbox:        QtWidgets.QVBoxLayout
+#  - __runnable:    FileInfo
+#  - __button_widget: QtWidgets.QWidget
+
+# TODO: Inherit the current class from QtWidgets.QDialog.
+# TODO: Create constructor for RunnerScreen class, that sets all variables above.
+# TODO: Create __init_ui(searched_runnables: List) method, that initializes the UI.
+# TODO: Implement the __run_program() method. This should execute the program.
+# TODO: Implement the __back() method, that initialize a new RunnableConfigScreen with the same runnable.
