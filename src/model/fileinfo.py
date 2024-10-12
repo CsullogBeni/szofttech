606cb1d500e42fd1fdecc09202c9b03d54473e11
Merth Borbála <borka.merth14@gmail.com>
2024-10-12 13:15:34 +0200

    docstring fix

--- src/model/fileinfo.py
+++ src/model/fileinfo.py
@@ -1,17 +1,16 @@
 from typing import List # for the argument list
 from argument import Argument  # to have access to argument class
 
-"""
-FileInfo class containing the following private variables:
- - __prog_path:           str
- - __prog_name:           str
- - __prog_description:    str
- - __args:                List[Argument]
- - __is_main_runnable:    bool
-"""
-class FileInfo:
+class FileInfo:    
     """
-    Constructor for FileInfo class, that sets all variables above.
+    Contains all the data about a runnable.
+    
+    Attributes:
+    - __prog_path:           the path to the runnable
+    - __prog_name:           the name of the runnable
+    - __prog_description:    the description of a runnable
+    - __args:                the argument list of the runnable
+    - __is_main_runnable:    indicates if it is a main runnable
     """
     def __init__(self, prog_path: str, prog_name: str, prog_description: str, args: List[Argument], is_main_runnable: bool):
         self.__prog_path = prog_path
@@ -20,31 +19,58 @@ class FileInfo:
         self.__args = args
         self.__is_main_runnable = is_main_runnable
 
-    """
-    Getters for all the variables in FileInfo class.
-    """
     def get_prog_path(self):
+        """
+        Getter for the file path of the runnable.
+
+        Returns:
+            str: the path of the runnable
+        """
         return self.__prog_path
 
     def get_prog_name(self):
+        """
+        Getter for the name of the runnable.
+
+        Returns:
+            str: the name of the runnable
+        """
         return self.__prog_name
 
     def get_prog_description(self):
+        """
+        Getter for the description of the runnable.
+
+        Returns:
+            str: the description of the runnable
+        """
         return self.__prog_description
 
     def get_args(self):
+        """
+        Getter for the argument list of the runnable.
+
+        Returns:
+            List[Argument]: the argument list of the runnable
+        """
         return self.__args
 
     def is_main_runnable(self):
-        return self.__is_main_runnable
-    """
-    End of getter section.
-    """
+        """
+        Getter for the main runnable flag of the runnable.
 
-    """
-    add_argument(arg: Argument) method that adds an argument to the argument list.
-    """
+        Returns:
+            bool: the main runnable flag of the runnable
+        """
+        return self.__is_main_runnable
+    
     def add_argument(self, arg: Argument):
+        """
+        Adds an argument to the argument list if it's not already added.
+
+        Args:
+            arg (Argument): the argument to be added to the argument list
+        """
         contains = false
         for _arg in __args:
             if _arg == arg:
@@ -52,8 +78,11 @@ class FileInfo:
         if not contains:
             self.__args.append(arg)
 
-    """
-    Setter function for __is_main_runnable variable.
-    """
     def set_main_runnable(self, is_main_runnable: bool):
+        """
+        Setter for the main runnable flag
+
+        Args:
+            is_main_runnable (bool): the value to which the flag must be set
+        """
         self.__is_main_runnable = is_main_runnable
