8aeb4cf1eedeebd98b3027e731c3647fc2835c22
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-10-13 18:23:16 +0200

    IDataAcces errors fixed

--- src/persistence/idata_access.py
+++ src/persistence/idata_access.py
@@ -2,20 +2,28 @@ from abc import ABC, abstractmethod
 
 
 class IDataAccess(ABC):
+    """
+    Abstract class for data access classes.
+
+    The classes that implement this interface should provide methods for saving and loading
+    configurations of the runnable programs and the list of main runnables.
+    """
     @abstractmethod
-    def save_config(runnable: str, data: dict):
+    def save_config(self, runnable: str, data: dict):
         pass
+
     @abstractmethod
-    def load_config(runnable: str) -> dict:
+    def load_config(self, runnable: str) -> dict:
         pass
+
     @abstractmethod
-    def save_main_runnables(runnables: dict):
+    def save_main_runnables(self, runnables: dict):
         pass
+
     @abstractmethod
-    def load_main_runnables() -> dict:
+    def load_main_runnables(self) -> dict:
         pass
+
     @abstractmethod
-    def clear_history():
+    def clear_history(self):
         pass
-    
-    
