417f21046bb2dd82891f696ade9af35d9667a53c
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-10-15 18:50:46 +0200

    test__argument_visitor.py file added

--- tests/unit_tests/model/test__argument_visitor.py
+++
@@ -1,7 +0,0 @@
-# TODO: Create unit tests for ArgumentVisitor class. Name the test class as TestArgumentVisitor.
-#  - Consider using builtin unittest library.
-#  - Inherit the new class from unittest.TestCase.
-#  - Create white box tests for all the getters and setters and functions.
-#  - Indicate, that the file is runnable(, aka at the end of the code:
-#                                         if __name__ == '__main__':
-#                                            unittest.main()
