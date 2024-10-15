2c29cc1d9f4b164ea9d839219ce480ffcaee9a99
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-10-15 18:59:10 +0200

    calc extract_argument tests added

--- tests/unit_tests/model/test__argument_visitor.py
+++ tests/unit_tests/model/test__argument_visitor.py
@@ -23,6 +23,22 @@ class TestArgumentVisitor(unittest.TestCase):
         The test checks if the extract_arguments function can extract the program name, description and argument details
         from the calculator.py file.
         """
+        calculator_path = os.path.join(working_dir_path, 'calculator', 'calculator.py')
+        prog, desc, args = extract_arguments(calculator_path)
+        self.assertEqual(prog, 'Calculator')
+        self.assertEqual(desc,
+                         'This program can add, subtract, multiply and divide two numbers. Numbers and' +
+                         ' operators given from the user as the command line arguments. The result is ' +
+                         'printed to the console.')
+        self.assertEqual(len(args), 3)
+        self.assertEqual(args[0], ('--number1', None, None, 'First number to do operation with', 'float', True, None,
+                                   []))
+        self.assertEqual(args[1], ('number2', None, None, 'Second number to do operation with', 'float', False, None,
+                                   []))
+        self.assertEqual(args[2], ('--operation', None, '+', 'Operation, which can be: +, -, *, /', None, False, None,
+                                   ['+', '-', '*', '/']))
+
+
 
 
 if __name__ == '__main__':
