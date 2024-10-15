aa3047b482697f614c09bb94bed58f47ab1ffdf2
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-10-15 19:00:07 +0200

    minmax extract arguemnts test added

--- tests/unit_tests/model/test__argument_visitor.py
+++ tests/unit_tests/model/test__argument_visitor.py
@@ -38,7 +38,27 @@ class TestArgumentVisitor(unittest.TestCase):
         self.assertEqual(args[2], ('--operation', None, '+', 'Operation, which can be: +, -, *, /', None, False, None,
                                    ['+', '-', '*', '/']))
 
+    def test__extract_arguments_minmax(self):
+        """
+        Test if the extract_arguments function can extract the program name, description and argument details from the
+        minmax.py file. The minmax.py file is a simple python script, which can select the maximum or the minimum from
+        the given numbers.
 
+        The test checks if the extract_arguments function can extract the program name, description and argument details
+        from the minmax.py file.
+        """
+        minmax_path = os.path.join(working_dir_path, 'minmax', 'minmax.py')
+        prog, desc, args = extract_arguments(minmax_path)
+        self.assertEqual(prog, 'Minmax')
+        self.assertEqual(desc,
+                         'This program can select the maximum or the minimum from the given numbers. Numbers' +
+                         ' given from the user as the command line arguments. The program select maximum by ' +
+                         'default, but user can ask minimum by --min flag. The result is printed to the console.')
+        self.assertEqual(len(args), 2)
+        self.assertEqual(args[0], ('--min', '-m', None, 'Whether user wants minimum selection', None, False,
+                                   'store_true', []))
+        self.assertEqual(args[1], ('numbers', None, None, 'Numbers to select maximum or minimum from', 'float',
+                                   False, None, []))
 
 
 if __name__ == '__main__':
