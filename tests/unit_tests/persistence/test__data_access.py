707fd173a5f3a1c9ee18a57fa1edaa23c46f3e20
dannajhun1 <daniel.gergely2000@gmail.com>
2024-10-20 15:44:11 +0200

    path update

--- tests/unit_tests/persistence/test__data_access.py
+++ tests/unit_tests/persistence/test__data_access.py
@@ -5,11 +5,13 @@
 #  - Indicate, that the file is runnable(, aka at the end of the code:
 #                                         if __name__ == '__main__':
 #                                            unittest.main()
+import unittest
 import os
 import pathlib
-import unittest
-
-# Full path to the test files directory
-working_dir_path = os.path.join(pathlib.Path(__file__).parent.parent.parent.resolve(), 'test_files')
+import json
+from src.persistence.data_access import DataAccess
 
 class TestDataAccess(unittest.TestCase):
+    def setUp(self):
+        self.data_path = os.path.join(pathlib.Path(__file__).parent.parent.parent.resolve(), 'src', 'persistence')
+        self.data_access = DataAccess(self.data_path)
