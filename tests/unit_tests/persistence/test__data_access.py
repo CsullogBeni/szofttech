698316264d4eed504a8d86d6dd6334eb2eea1978
dannajhun1 <daniel.gergely2000@gmail.com>
2024-10-20 15:51:46 +0200

    added test_save_config

--- tests/unit_tests/persistence/test__data_access.py
+++ tests/unit_tests/persistence/test__data_access.py
@@ -13,5 +13,16 @@ from src.persistence.data_access import DataAccess
 
 class TestDataAccess(unittest.TestCase):
     def setUp(self):
-        self.data_path = os.path.join(pathlib.Path(__file__).parent.parent.parent.resolve(), 'src', 'persistence')
-        self.data_access = DataAccess(self.data_path)
+        self.data_path = pathlib.Path(pathlib.Path(__file__).parent.parent.parent.resolve() / 'src' / 'persistence')
+        self.data_access = DataAccess(str(self.data_path))
+
+    def test_save_config(self):
+        # Test the save_config method
+        runnable = "test_runnable"
+        data = {"key": "value"}
+        self.data_access.save_config(runnable, data)
+        expected_file_path = self.data_path / "SZOFTECH" / f"{runnable.replace('/', '_').replace('\'', '_')}.json"
+        self.assertTrue(os.path.exists(expected_file_path))
+        with open(expected_file_path, 'r') as json_file:
+            actual_data = json.load(json_file)
+        self.assertEqual(actual_data, data)
