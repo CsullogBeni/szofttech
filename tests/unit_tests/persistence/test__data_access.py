ceb0da0aaccd1914dc23f7373698858d1950649e
dannajhun1 <daniel.gergely2000@gmail.com>
2024-10-20 22:20:49 +0200

    pep8 fix & expected_file_path fix

--- tests/unit_tests/persistence/test__data_access.py
+++ tests/unit_tests/persistence/test__data_access.py
@@ -1,15 +1,9 @@
-# TODO: Create unit tests for DataAccess class. Name the test class as TestDataAccess.
-#  - Consider using builtin unittest library.
-#  - Inherit the new class from unittest.TestCase.
-#  - Create white box tests for all the getters and setters and functions.
-#  - Indicate, that the file is runnable(, aka at the end of the code:
-#                                         if __name__ == '__main__':
-#                                            unittest.main()
 import unittest
 import os
 import pathlib
 import json
 from src.persistence.data_access import DataAccess
+
 
 class TestDataAccess(unittest.TestCase):
     def setUp(self):
@@ -28,7 +22,7 @@ class TestDataAccess(unittest.TestCase):
         runnable = "test_runnable"
         data = {"key": "value"}
         self.data_access.save_config(runnable, data)
-        expected_file_path = self.data_path / "SZOFTECH" / f"{runnable.replace('/', '_').replace('\'', '_')}.json"
+        expected_file_path = self.data_path / "SZOFTECH" / (runnable.replace('/', '').replace(",", '_') + ".json")
         self.assertTrue(os.path.exists(expected_file_path))
         with open(expected_file_path, 'r') as json_file:
             actual_data = json.load(json_file)
@@ -93,6 +87,7 @@ class TestDataAccess(unittest.TestCase):
         self.data_access.clear_history()
         expected_file_path = self.data_path / "SZOFTECH" / "main_runnables.json"
         self.assertFalse(os.path.exists(expected_file_path))
+
 
 if __name__ == '__main__':
     unittest.main()
