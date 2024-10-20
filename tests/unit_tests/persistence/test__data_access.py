0665ae6d0d1ae0d5cb2747a48bc3dab55952020e
dannajhun1 <daniel.gergely2000@gmail.com>
2024-10-20 15:15:29 +0200

    test_data_access init

--- tests/unit_tests/persistence/test__data_access.py
+++ tests/unit_tests/persistence/test__data_access.py
@@ -5,3 +5,11 @@
 #  - Indicate, that the file is runnable(, aka at the end of the code:
 #                                         if __name__ == '__main__':
 #                                            unittest.main()
+import os
+import pathlib
+import unittest
+
+# Full path to the test files directory
+working_dir_path = os.path.join(pathlib.Path(__file__).parent.parent.parent.resolve(), 'test_files')
+
+class TestDataAccess(unittest.TestCase):
