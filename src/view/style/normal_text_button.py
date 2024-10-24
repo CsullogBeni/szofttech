f9ee0ed4b4727fc663638342e5350bd7fae43575
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-10-24 17:26:34 +0200

    Docstring added

--- src/view/style/normal_text_button.py
+++ src/view/style/normal_text_button.py
@@ -3,6 +3,9 @@ from PyQt5.QtGui import QFont
 
 
 class NormalTextButton(QPushButton):
+    """
+    Subclass of QPushButton, that sets the font, text and tool tip.
+    """
     def __init__(self, text: str = '', tool_tip: str = None, font_size: int = 10):
         super(NormalTextButton, self).__init__()
         self.setText(text)
