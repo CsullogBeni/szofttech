9844811e599af4157ecadc77e5e06650ae6aa658
Laczkó Zsófia <jizacc@INF.ELTE.HU>
2024-10-25 19:40:34 +0200

    Add docstring for NormalTextLabel

--- src/view/style/normal_text_label.py
+++ src/view/style/normal_text_label.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import QLabel
 
 
 class NormalTextLabel(QLabel):
+    """
+    Subclass of QLabel, that sets the text and the font style.
+    """
+
     def __init__(self, text: str = '', font_size: int = 12):
         super(NormalTextLabel, self).__init__()
         self.setText(text)
