bc042a391aab52a43e06f4689e794ab86b88de2d
dannajhun1 <daniel.gergely2000@gmail.com>
2024-10-27 15:11:40 +0100

    main

--- src/view/main.py
+++ src/view/main.py
@@ -8,3 +8,34 @@
 #  - Indicate, that the file is runnable(, aka at the end of the code:
 #                                         if __name__ == '__main__':
 #                                            main()
+
+import sys
+import pathlib
+import os
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+
+from src.model.model import Model
+from src.view.show_runnables_screen import ShowRunnablesScreen
+
+
+def main():
+    """
+    Main function of the program. Initialize the User interface.
+    """
+    app = QApplication(sys.argv)
+    app.setApplicationName('PyRun')
+    widget = QtWidgets.QStackedWidget()
+    model = Model()
+    project_path = pathlib.Path(__file__).parent.parent.parent.absolute()
+    showrunnablescreen = ShowRunnablesScreen(model, widget, str(project_path) + '/tests/test_files')
+    widget.addWidget(showrunnablescreen)
+    widget.setFixedWidth(1200)
+    widget.setFixedHeight(800)
+    widget.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
