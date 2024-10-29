e1052fc071c6e69e0b64b7aa51acb074727871aa
Márton Petes <tx0lwm@inf.elte.hu>
2024-10-29 15:31:55 +0100

    add main

--- src/view/main.py
+++ src/view/main.py
@@ -5,3 +5,20 @@ from PyQt5.QtWidgets import QApplication
 
 from src.model.model import Model
 from src.view.show_runnables_screen import ShowRunnablesScreen
+
+def main():
+
+    app = QApplication(sys.argv)
+    app.setApplicationName('PyRun')
+    widget = QtWidgets.QStackedWidget()
+    model = Model()
+    show_runnable_screen = ShowRunnablesScreen(model, widget)
+    widget.addWidget(show_runnable_screen)
+    widget.setFixedWidth(1200)
+    widget.setFixedHeight(800)
+    widget.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
