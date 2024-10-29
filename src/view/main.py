7c6bdcf6290bea254fcece7e881d267519146883
Márton Petes <tx0lwm@inf.elte.hu>
2024-10-29 15:31:02 +0100

    add scaffolding

--- src/view/main.py
+++ src/view/main.py
@@ -1,10 +1,7 @@
-# TODO: Create the main function of the program.
-#  - Import QApplication and QtWidgets from PyQt5 library, name it as app.
-#  - main function should initialize a QApplication object, name it as widget.
-#  - main function should initialize a QtWidgets.QStackedWidget, it will used for changing windows.
-#  - main function should initialize a Model object, that will be used in the whole runtime.
-#  - main function should call widget.show().
-#  - Don't forget to write to the end of main the following: sys.exit(app.exec_()9
-#  - Indicate, that the file is runnable(, aka at the end of the code:
-#                                         if __name__ == '__main__':
-#                                            main()
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+
+from src.model.model import Model
+from src.view.show_runnables_screen import ShowRunnablesScreen
