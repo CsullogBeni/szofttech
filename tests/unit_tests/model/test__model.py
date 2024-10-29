3c6a208c2168469d9243b82040db9147022b0a78
Márton Petes <tx0lwm@inf.elte.hu>
2024-10-29 14:07:28 +0100

    modell class scaffolding

--- tests/unit_tests/model/test__model.py
+++ tests/unit_tests/model/test__model.py
@@ -1,7 +1,13 @@
-# TODO: Create unit tests for Model class. Name the test class as TestModel.
-#  - Consider using builtin unittest library.
-#  - Inherit the new class from unittest.TestCase.
-#  - Create white box tests for all the getters and setters and functions.
-#  - Indicate, that the file is runnable(, aka at the end of the code:
-#                                         if __name__ == '__main__':
-#                                            unittest.main()
+import os
+import pathlib
+import sys
+import unittest
+
+from src.model.model import Model
+from src.model.fileinfo import FileInfo
+from src.model.argument import Argument
+
+test_path = os.path.join(pathlib.Path(__file__).resolve().parent.parent.parent, 'test_files')
+
+
+class TestModel(unittest.TestCase):
