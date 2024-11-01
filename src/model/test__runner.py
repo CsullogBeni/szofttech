0c70cc7de498586c3e626e97634cc41c70538984
dannajhun1 <daniel.gergely2000@gmail.com>
2024-11-01 19:51:36 +0100

    run_tests method

--- src/model/test__runner.py
+++ src/model/test__runner.py
@@ -3,3 +3,18 @@ import argparse
 
 from src.model.fileinfo import FileInfo
 from src.model.model import Model
+
+def main() -> None:
+    args = parse_args()
+
+    if args.path:
+        if not os.path.isdir(args.path):
+            raise AttributeError(f"Invalid path: {args.path}")
+
+    run_tests(args.path)
+
+def run_tests(path: str) -> None:
+    model = Model()
+    model.add_working_directory_path(path)
+    for runnable in model.get_runnables:
+        execute_test(runnable, model)
