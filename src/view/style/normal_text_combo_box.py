f49de6e09cdbc21ec76ec1478c923d888664f4b9
Merth Borbála <borka.merth14@gmail.com>
2024-11-06 19:54:36 +0100

    normal text combo box widget custom settings

--- src/view/style/normal_text_combo_box.py
+++ src/view/style/normal_text_combo_box.py
@@ -7,3 +7,18 @@ class NormalTextComboBox(QComboBox):
     """
     def __init__(self, choices: list = None, arg_id: str = '', default: str = None, font_size: int = 12):
         super(NormalTextComboBox, self).__init__()
+        if choices is None:
+            choices = []
+        self.addItem("")
+        tool_tip = None
+        if arg_id and default:
+            tool_tip = arg_id + '\nDefault: ' + str(default)
+        elif arg_id:
+            tool_tip = arg_id
+        for choice in choices:
+            self.addItem(str(choice))
+            if arg_id:
+                tool_tip += f"\n{str(choice)}"
+        if arg_id:
+            self.setToolTip(tool_tip)
+        self.setFont(QFont('MS shell Dlg 2', font_size))
