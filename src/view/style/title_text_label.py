c44d4d51bd368e10d42394b456d1c2bc3d5476de
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-11-17 19:42:03 +0100

    font size changed

--- src/view/style/title_text_label.py
+++ src/view/style/title_text_label.py
@@ -5,7 +5,7 @@ class TitleTextLabel(QLabel):
     """
     It is a subclass of QLabel, that sets the text and the font style to represent a title.
     """
-    def __init__(self, text: str = '', font_size: int = 12):
+    def __init__(self, text: str = '', font_size: int = 16):
         super(TitleTextLabel, self).__init__()
         self.setText(text)
         self.setFont(QFont('MS shell Dlg 2', font_size))
