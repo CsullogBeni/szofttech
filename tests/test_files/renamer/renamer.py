c081258d7c906adc6e61a4a2723a025adfb9e309
Csüllög Benedek Szabolcs <bvdorj@inf.elte.hu>
2024-11-22 15:39:57 +0100

    rename directory function added

--- tests/test_files/renamer/renamer.py
+++ tests/test_files/renamer/renamer.py
@@ -58,3 +58,30 @@ def _rename_file(full_path: str, new_name: str, directory: str, overwrite: bool 
     os.rename(full_path, os.path.join(directory, new_name))
 
 
+def _rename_directory(full_path: str, new_name: str, directory: str, overwrite: bool = False) -> None:
+    """
+    Renames a directory.
+
+    Args:
+        full_path (str): The full path to the directory to be renamed.
+        new_name (str): The new name for the directory.
+        directory (str): The directory of the file to be renamed.
+        overwrite (bool, optional): Whether to overwrite the existing directory or not. Defaults to False.
+
+    Raises:
+        IOError: If the path is not a directory.
+        ValueError: If the new name contains '.'.
+        FileExistsError: If the directory already exists.
+    """
+    if not os.path.isdir(full_path):
+        raise IOError(f"Path is not a directory: {full_path}")
+    if '.' in new_name:
+        raise ValueError(f"New name cannot contain '.': {new_name}")
+    if overwrite:
+        if os.path.exists(os.path.join(directory, new_name)):
+            os.remove(os.path.join(directory, new_name))
+    else:
+        if os.path.exists(os.path.join(os.path.dirname(full_path), new_name)):
+            raise FileExistsError(f"Directory already exists: {os.path.join(os.path.dirname(full_path), new_name)}")
+    os.rename(full_path, os.path.join(directory, new_name))
+
