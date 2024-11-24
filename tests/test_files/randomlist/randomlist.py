d2e97b50936279b49e0f8e0f048ced7078906f73
Márton Petes <tx0lwm@inf.elte.hu>
2024-11-24 01:20:15 +0100

    add implementation

--- tests/test_files/randomlist/randomlist.py
+++ tests/test_files/randomlist/randomlist.py
@@ -1,15 +1,21 @@
 import argparse
+from random import randint
 
 def parse_arguments():
-  parser = argparse.ArgumentParser*(
+  parser = argparse.ArgumentParser()
   parser.prog = "Random List Generator"
   parser.description = "Generates a random list of integers"
-  parser.add_argument("--count", "-c" type=int, help="How many integers to generate", required=True)
+  parser.add_argument("--count", "-c", type=int, help="How many integers to generate", required=True)
   parser.add_argument("--lowerbound", "-lb", type=int, help="Lower bound, defaults to 0", default=0)
   parser.add_argument("--upperbound", "-ub", type=int, help="Upper bound, defaults to the integer limit", default=2147483647)
 
   return parser.parse_args()
 
+def generate(args):
+ l = [randint(args.lowerbound, args.upperbound) for _ in range(1, args.count)]
+ print(*l, sep=' ')
 
-if __name__ = "__main__":
+
+if __name__ == "__main__":
   parsed_args = parse_arguments()
+  generate(parsed_args)
