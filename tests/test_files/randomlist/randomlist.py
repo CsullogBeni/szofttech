06f0f35c8b596c9d5f45ac856875945fcb79dd87
Márton Petes <tx0lwm@inf.elte.hu>
2024-11-24 01:13:13 +0100

    add randomlist.py

---
+++ tests/test_files/randomlist/randomlist.py
@@ -0,0 +1,11 @@
+import argparse
+
+def parse_arguments():
+  parser = argparse.ArgumentParser*(
+  parser.prog = "Random List Generator"
+  parser.description = "Generates a random list of integers"
+  parser.add_argument("--count", "-c" type=int, help="How many integers to generate", required=True)
+  parser.add_argument("--lowerbound", "-lb", type=int, help="Lower bound, defaults to 0", default=0)
+  parser.add_argument("--upperbound", "-ub", type=int, help="Upper bound, defaults to the integer limit", default=2147483647)
+
+  return parser.parse_args()
